cab422df271b8e70d151d7e237948fd6bd1e0d04
pak-ji <pakji@tsnlab.com>
2026-04-02 15:32:02 +0900

    Fix file descriptor error

--- services/tsnlab_echo/tsnlab_echo.c
+++ services/tsnlab_echo/tsnlab_echo.c
@@ -25,51 +25,48 @@
 #include "simple_api.h"
 #include "debug.h"
 
-int tsnlab_echo_init_service(ci_service_xdata_t * srv_xdata,
-                      struct ci_server_conf *server_conf);
-int tsnlab_echo_check_preview_handler(char *preview_data, int preview_data_len,
-                               ci_request_t *);
+#include <time.h>
+#include <errno.h>
+#include <sys/time.h>
+#include <pwd.h>
+#include <grp.h>
+#include <unistd.h>
+
+#define ASSETS_PATH "/var/log/c-icap/assets"
+#define FILENAME_MAX_LEN 64
+
+static const char *get_extension_from_content_type(ci_request_t * req);
+static char *generate_unique_filename(const char *ext);
+
+int tsnlab_echo_init_service(ci_service_xdata_t * srv_xdata, struct ci_server_conf *server_conf);
+int tsnlab_echo_check_preview_handler(char *preview_data, int preview_data_len, ci_request_t *);
 int tsnlab_echo_end_of_data_handler(ci_request_t * req);
 void *tsnlab_echo_init_request_data(ci_request_t * req);
 void tsnlab_echo_close_service();
 void tsnlab_echo_release_request_data(void *data);
-int tsnlab_echo_io(char *wbuf, int *wlen, char *rbuf, int *rlen, int iseof,
-            ci_request_t * req);
-
+int tsnlab_echo_io(char *wbuf, int *wlen, char *rbuf, int *rlen, int iseof, ci_request_t * req);
 
 CI_DECLARE_MOD_DATA ci_service_module_t service = {
-    "tsnlab_echo",                         /* mod_name, The module name */
-    "TSNLAB Echo demo service",            /* mod_short_descr,  Module short description */
-    ICAP_RESPMOD | ICAP_REQMOD,     /* mod_type, The service type is responce or request modification */
-    tsnlab_echo_init_service,              /* mod_init_service. Service initialization */
-    NULL,                           /* post_init_service. Service initialization after c-icap
-                    configured. Not used here */
-    tsnlab_echo_close_service,           /* mod_close_service. Called when service shutdowns. */
-    tsnlab_echo_init_request_data,         /* mod_init_request_data */
-    tsnlab_echo_release_request_data,      /* mod_release_request_data */
-    tsnlab_echo_check_preview_handler,     /* mod_check_preview_handler */
-    tsnlab_echo_end_of_data_handler,       /* mod_end_of_data_handler */
-    tsnlab_echo_io,                        /* mod_service_io */
-    NULL,
-    NULL
+    "tsnlab_echo",                      /* mod_name, The module name */
+    "TSNLAB Echo demo service",         /* mod_short_descr,  Module short description */
+    ICAP_RESPMOD | ICAP_REQMOD,         /* mod_type, The service type is responce or request modification */
+    tsnlab_echo_init_service,           /* mod_init_service. Service initialization */
+    NULL,                               /* post_init_service. Service initialization after c-icap configured. Not used here */
+    tsnlab_echo_close_service,          /* mod_close_service. Called when service shutdowns. */
+    tsnlab_echo_init_request_data,      /* mod_init_request_data */
+    tsnlab_echo_release_request_data,   /* mod_release_request_data */
+    tsnlab_echo_check_preview_handler,  /* mod_check_preview_handler */
+    tsnlab_echo_end_of_data_handler,    /* mod_end_of_data_handler */
+    tsnlab_echo_io,                     /* mod_service_io */
+    NULL, NULL
 };
 
-/*
-  The tsnlab_echo_req_data structure will store the data required to serve an ICAP request.
-*/
 struct tsnlab_echo_req_data {
-    /*the body data*/
-    ci_ring_buf_t *body;
-    /*flag for marking the eof*/
-    int eof;
-
-#if 1
     ci_simple_file_t *file_t;
-#endif
+    ci_ring_buf_t *body;
+    int eof;
 };
 
-
-/* This function will be called when the service loaded  */
 int tsnlab_echo_init_service(ci_service_xdata_t * srv_xdata,
                       struct ci_server_conf *server_conf)
 {
@@ -88,109 +85,79 @@ int tsnlab_echo_init_service(ci_service_xdata_t * srv_xdata,
       which contains the username and the groups in which belongs.  */
     ci_service_set_xopts(srv_xdata,  CI_XAUTHENTICATEDUSER|CI_XAUTHENTICATEDGROUPS);
 
+    /* Create assets directory if it doesn't exist */
+    if (access(ASSETS_PATH, F_OK) == -1) {
+        if (mkdir(ASSETS_PATH, 0755) < 0 && errno != EEXIST) {
+            ci_debug_printf(1, "TSNLAB: Failed to create assets directory: %s, error: %s\n", ASSETS_PATH, strerror(errno));
+            return CI_ERROR;
+        }
+    }
+
+    /* Set owner and group to c-icap */
+    if (access(ASSETS_PATH, F_OK) == 0) {
+        struct passwd *pwd = getpwnam("c-icap");
+        struct group *grp = getgrnam("c-icap");
+        if (pwd && grp) {
+            if (chown(ASSETS_PATH, pwd->pw_uid, grp->gr_gid) < 0) {
+                ci_debug_printf(1, "TSNLAB: Failed to chown %s to c-icap:c-icap: %s\n", ASSETS_PATH, strerror(errno));
+            }
+        } else {
+            ci_debug_printf(1, "TSNLAB: c-icap user or group not found, skip chown\n");
+        }
+    }
+
     return CI_OK;
 }
 
-/* This function will be called when the service shutdown */
 void tsnlab_echo_close_service()
 {
     ci_debug_printf(5,"Service shutdown!\n");
-    /*Nothing to do*/
 }
 
-/*This function will be executed when a new request for tsnlab_echo service arrives. This function will
-  initialize the required structures and data to serve the request.
- */
 void *tsnlab_echo_init_request_data(ci_request_t * req)
 {
     struct tsnlab_echo_req_data *tsnlab_echo_data;
 
-    /*Allocate memory fot the tsnlab_echo_data*/
     tsnlab_echo_data = malloc(sizeof(struct tsnlab_echo_req_data));
     if (!tsnlab_echo_data) {
         ci_debug_printf(1, "Memory allocation failed inside tsnlab_echo_init_request_data!\n");
         return NULL;
     }
 
-    /*If the ICAP request encuspulates a HTTP objects which contains body data
-      and not only headers allocate a ci_cached_file_t object to store the body data.
-     */
-    if (ci_req_hasbody(req)) {
+    tsnlab_echo_data->file_t = NULL;
+    if (ci_req_hasbody(req))
         tsnlab_echo_data->body = ci_ring_buf_new(4096);
-#if 1 // jihoon
-        #define DIR_PATH "/var/log/c-icap/"
-        if (req->type == ICAP_RESPMOD) {
-            tsnlab_echo_data->file_t = ci_simple_file_new(0);
-            //tsnlab_echo_data->file_t = ci_simple_file_named_new(DIR_PATH, "0000.txt", 0);
-            if(!tsnlab_echo_data->file_t)
-                ci_debug_printf(1, "TSNLAB: failed create the file: fd=%d\n", tsnlab_echo_data->file_t->fd);
-        }
-        else {
-            tsnlab_echo_data->file_t = NULL;
-        }
-#endif
-    }
-    else {
+    else
         tsnlab_echo_data->body = NULL;
-#if 1 // jihoon
-        tsnlab_echo_data->file_t = NULL;
-#endif
-    }
-
     tsnlab_echo_data->eof = 0;
-    /*Return to the c-icap server the allocated data*/
+
     return tsnlab_echo_data;
 }
 
-/*This function will be executed after the request served to release allocated data*/
 void tsnlab_echo_release_request_data(void *data)
 {
-    /*The data points to the tsnlab_echo_req_data struct we allocated in function tsnlab_echo_init_service */
     struct tsnlab_echo_req_data *tsnlab_echo_data = (struct tsnlab_echo_req_data *)data;
 
-    /*if we had body data, release the related allocated data*/
     if (tsnlab_echo_data->body)
         ci_ring_buf_destroy(tsnlab_echo_data->body);
-#if 1 // jihoon
-    if (tsnlab_echo_data->file_t) {
-        ci_simple_file_destroy(tsnlab_echo_data->file_t);
-        //ci_simple_file_release(tsnlab_echo_data->file_t);
-    }
-#endif
+    if (tsnlab_echo_data->file_t)
+        ci_simple_file_release(tsnlab_echo_data->file_t);
 
     free(tsnlab_echo_data);
 }
 
-
-static int whattodo = 0;
 int tsnlab_echo_check_preview_handler(char *preview_data, int preview_data_len,
                                ci_request_t * req)
 {
-    ci_off_t content_len;
-
-    /*Get the tsnlab_echo_req_data we allocated using the  tsnlab_echo_init_service  function*/
+    int ret;
     struct tsnlab_echo_req_data *tsnlab_echo_data = ci_service_data(req);
-
-    /*If there are is a Content-Length header in encupsulated Http object read it
-     and display a debug message (used here only for debuging purposes)*/
-    content_len = ci_http_content_length(req);
-    ci_debug_printf(1, "TSNLAB: We expect to read :%" PRINTF_OFF_T " body data\n",
-                    (CAST_OFF_T) content_len);
-
-    /*If there are not body data in HTTP encapsulated object but only headers
-      respond with Allow204 (no modification required) and terminate here the
-      ICAP transaction */
+    
     if (!ci_req_hasbody(req)) {
-        ci_debug_printf(1, "TSNLAB: not ci_req_hasbody to CI_MOD_ALLOW204\n");
         return CI_MOD_ALLOW204;
     }
 
-    /*Unlock the request body data so the c-icap server can send data before
-      all body data has received */
     ci_req_unlock_data(req);
 
-    /*If there are not preview data tell to the client to continue sending data
-      (http object modification required). */
     if (!preview_data_len)
         return CI_MOD_CONTINUE;
 
@@ -200,85 +167,208 @@ int tsnlab_echo_check_preview_handler(char *preview_data, int preview_data_len,
     object headers or/and the preview_data buffer.
 
     In this example service we just use the whattodo static variable to decide
-    if we want to process or not the HTTP object.
-         */
-    if (whattodo == 0) {
-        whattodo = 1;
-        ci_debug_printf(1, "TSNLAB: tsnlab_echo service will process the request\n");
+    if we want to process or not the HTTP object. */
 
-        /*if we have preview data and we want to proceed with the request processing
-          we should store the preview data. There are cases where all the body
-          data of the encapsulated HTTP object included in preview data. Someone can use
-          the ci_req_hasalldata macro to  identify these cases*/
-        if (preview_data_len) {
-            ci_ring_buf_write(tsnlab_echo_data->body, preview_data, preview_data_len);
-            tsnlab_echo_data->eof = ci_req_hasalldata(req);
-#if 1 // jihoon
-            ci_debug_printf(1, "TSNLAB: exist preview_data_len\n");
-            ci_simple_file_write(tsnlab_echo_data->file_t, preview_data, preview_data_len, tsnlab_echo_data->eof);
-#endif
-        }
-        return CI_MOD_CONTINUE;
-    } else {
-        whattodo = 0;
-        /*Nothing to do just return an allow204 (No modification) to terminate here
-         the ICAP transaction */
-        ci_debug_printf(1, "Allow 204...\n");
+    if (req->type == ICAP_REQMOD) {
         return CI_MOD_ALLOW204;
     }
+
+    if (preview_data_len && req->type == ICAP_RESPMOD) {
+        char *filename;
+        const char *ext;
+
+        tsnlab_echo_data->eof = ci_req_hasalldata(req);
+        ext = get_extension_from_content_type(req);
+ 
+        /** 
+         * If not html, return 204.
+         * We only support html files for now.
+         *
+         * TODO: Add support for other file types 
+         */
+        if (strcmp(ext, "html")) {
+            return CI_MOD_ALLOW204;
+        }
+
+        filename = generate_unique_filename(ext);
+        if (!filename) {
+            return CI_ERROR;
+        }
+        
+        /* Create file in assets directory */
+        tsnlab_echo_data->file_t = ci_simple_file_named_new((char *)ASSETS_PATH, filename, 0);
+        free(filename);
+
+        if (!tsnlab_echo_data->file_t) {
+            ci_debug_printf(1, "Failed to create file in %s\n", ASSETS_PATH);
+            return CI_ERROR;
+        }
+
+        if (tsnlab_echo_data->body)
+            ci_ring_buf_write(tsnlab_echo_data->body, preview_data, preview_data_len);
+
+        ret = ci_simple_file_write(tsnlab_echo_data->file_t, preview_data, preview_data_len, tsnlab_echo_data->eof);
+        if (ret < 0) {
+            ci_debug_printf(1, "Failed to write to file in %s\n", ASSETS_PATH);
+            return CI_ERROR;
+        }
+
+        if (tsnlab_echo_data->eof) {
+            ci_simple_file_release(tsnlab_echo_data->file_t);
+            return CI_MOD_DONE;
+        }
+    }
+
+    return CI_MOD_CONTINUE;
 }
 
-/* This function will called if we returned CI_MOD_CONTINUE in  tsnlab_echo_check_preview_handler
- function, after we read all the data from the ICAP client*/
 int tsnlab_echo_end_of_data_handler(ci_request_t * req)
 {
     struct tsnlab_echo_req_data *tsnlab_echo_data = ci_service_data(req);
 
-#if 1 // jihoon
-    ci_debug_printf(1, "File Length: %ld\n", tsnlab_echo_data->file_t->endpos);
-#endif
+    if (tsnlab_echo_data->file_t)
+        ci_debug_printf(1, "File Length: %ld\n", tsnlab_echo_data->file_t->endpos);
 
-    /*mark the eof*/
+    if (tsnlab_echo_data->file_t) {
+        ci_simple_file_release(tsnlab_echo_data->file_t);
+        tsnlab_echo_data->file_t = NULL;
+    }
+
     tsnlab_echo_data->eof = 1;
-    /*and return CI_MOD_DONE */
+
     return CI_MOD_DONE;
 }
 
-/* This function will called if we returned CI_MOD_CONTINUE in  tsnlab_echo_check_preview_handler
-   function, when new data arrived from the ICAP client and when the ICAP client is
-   ready to get data.
-*/
 int tsnlab_echo_io(char *wbuf, int *wlen, char *rbuf, int *rlen, int iseof,
             ci_request_t * req)
 {
     int ret;
     struct tsnlab_echo_req_data *tsnlab_echo_data = ci_service_data(req);
+
     ret = CI_OK;
 
-    /* Start echo: rbuf(origin) to wbuf(echo) */
+    /* Same as echo_io: inbound data goes into the ring buffer */
+    if (rlen && rbuf && req->type == ICAP_RESPMOD && req->hasbody) {
+        int rd = *rlen;
 
-    /*write the data read from icap_client to the tsnlab_echo_data->body*/
-    if (rlen && rbuf) {
-
-#if 1 // jihoon
-        if (req->hasbody && req->type == ICAP_RESPMOD) {
-            ret = ci_simple_file_write(tsnlab_echo_data->file_t, rbuf, *rlen, iseof);
-            //ci_debug_printf(1, "read data to file: %d\n", ret);
+        if (tsnlab_echo_data->body) {
+            *rlen = ci_ring_buf_write(tsnlab_echo_data->body, rbuf, rd);
+            if (*rlen < 0)
+                ret = CI_ERROR;
         }
-#endif
 
-        *rlen = ci_ring_buf_write(tsnlab_echo_data->body, rbuf, *rlen);
-        if (*rlen < 0)
-            ret = CI_ERROR;
+        if (ret == CI_OK && *rlen > 0) {
+            if (!tsnlab_echo_data->file_t) {
+                char *filename;
+                const char *ext = get_extension_from_content_type(req);
+
+                filename = generate_unique_filename(ext);
+                if (!filename) {
+                    return CI_ERROR;
+                }
+                tsnlab_echo_data->file_t = ci_simple_file_named_new((char *)ASSETS_PATH, filename, 0);
+                free(filename);
+
+                if (!tsnlab_echo_data->file_t) {
+                    ci_debug_printf(1, "Failed to create file in %s\n", ASSETS_PATH);
+                    return CI_ERROR;
+                }
+            }
+
+            if (ci_simple_file_write(tsnlab_echo_data->file_t, rbuf, *rlen, iseof) < 0) {
+                ci_debug_printf(1, "Failed to write to file in %s\n", ASSETS_PATH);
+                return CI_ERROR;
+            }
+        }
     }
 
-    /*read some data from the tsnlab_echo_data->body and put them to the write buffer to be send
-     to the ICAP client*/
-    if (wbuf && wlen) {
+    /* Echo client output from ring buffer only (no ci_simple_file_read) */
+    if (wbuf && wlen && tsnlab_echo_data->body) {
         *wlen = ci_ring_buf_read(tsnlab_echo_data->body, wbuf, *wlen);
         if (*wlen == 0 && tsnlab_echo_data->eof == 1)
             *wlen = CI_EOF;
     }
 
     return ret;
+}
+
+static char *generate_unique_filename(const char *ext)
+{
+    struct timespec ts;
+    struct tm tm;
+    struct timeval tv;
+    char *filename;
+
+    filename = malloc(FILENAME_MAX_LEN);
+    if (!filename) {
+        ci_debug_printf(5, "Failed to allocate filename buffer\n");
+        return NULL;
+    }
+
+    if (gettimeofday(&tv, NULL) < 0) {
+        free(filename);
+        ci_debug_printf(5, "gettimeofday failed\n");
+        return NULL;
+    }
+    ts.tv_sec = tv.tv_sec;
+    ts.tv_nsec = (long)tv.tv_usec * 1000;
+
+    if (!localtime_r(&ts.tv_sec, &tm)) {
+        free(filename);
+        ci_debug_printf(5, "localtime_r failed\n");
+        return NULL;
+    }
+
+    unsigned int random_number = rand();
+
+    snprintf(filename, FILENAME_MAX_LEN, "%04d%02d%02d_%02d%02d%02d_%09ld_%08X.%s",
+            tm.tm_year + 1900,
+            tm.tm_mon + 1,
+            tm.tm_mday,
+            tm.tm_hour,
+            tm.tm_min,
+            tm.tm_sec,
+            (long)ts.tv_nsec,
+            random_number,
+            ext ? ext : "bin");
+
+    return filename;
+}
+
+static const char *get_extension_from_content_type(ci_request_t * req)
+{
+    const char *ct;
+
+    if (req->type == ICAP_RESPMOD)
+        ct = ci_http_response_get_header(req, "Content-Type");
+    else
+        ct = ci_http_request_get_header(req, "Content-Type");
+
+    if (!ct)
+        return "bin";
+
+    if (strcasestr(ct, "text/html"))
+        return "html";
+    if (strcasestr(ct, "text/plain"))
+        return "txt";
+    if (strcasestr(ct, "text/css"))
+        return "css";
+    if (strcasestr(ct, "text/javascript") || strcasestr(ct, "application/javascript"))
+        return "js";
+    if (strcasestr(ct, "application/json"))
+        return "json";
+    if (strcasestr(ct, "image/jpeg") || strcasestr(ct, "image/jpg"))
+        return "jpg";
+    if (strcasestr(ct, "image/png"))
+        return "png";
+    if (strcasestr(ct, "image/gif"))
+        return "gif";
+    if (strcasestr(ct, "image/webp"))
+        return "webp";
+    if (strcasestr(ct, "application/xml") || strcasestr(ct, "text/xml"))
+        return "xml";
+    if (strcasestr(ct, "application/pdf"))
+        return "pdf";
+
+    return "bin";
 }
